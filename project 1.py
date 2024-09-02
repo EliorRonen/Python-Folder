837cd4015b98404cad7556bc0d0601255eee2add
EliorRonen <Ronenelior2014@gmail.com>
2024-09-02 17:24:37 +0300

    #python project

---
+++ project 1.py
@@ -0,0 +1,10 @@
+# the greeting meesage
+print("Hello Sneha")
+
+# my name
+# i know you know my name im just doing this for the project
+print("im elior")
+# a fun fact about myself
+print("i really like coding in python and i like math")
+# this is definitely a simple caculation
+print("the answer to 364.7 x 324.8 is 118,454.56")
