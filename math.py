c39773850763bacaabda0d2eccbdb61433ebf488
EliorRonen <Ronenelior2014@gmail.com>
2024-09-02 18:32:09 +0300

    math class

---
+++ math.py
@@ -0,0 +1,8 @@
+skibidi1 = 156.097895
+skibidi2 = 789.09726354
+skibidi3 = 986.54820
+skibidi4 = 72
+skibidi5 = 10
+python = skibidi1+skibidi2+skibidi3+skibidi4+skibidi5
+print("the word python is =",python)
+print("the avrege of python is =",python/5)
