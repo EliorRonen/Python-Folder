b8f97275593d0ef7f4ab6497e2b7ba0666ca758a
EliorRonen <Ronenelior2014@gmail.com>
2024-09-04 18:58:13 +0300

    python

---
+++ math part 3.py
@@ -0,0 +1,7 @@
+elior = int(input("enter the number:"))
+if elior < 10:
+    print("elior is smaller than 10")
+    print("elior is going to be 10 very soon")
+else:
+    print("elior is bigger than 10 congrats")
+    print("im not young anymore")
