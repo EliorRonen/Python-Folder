fc41b66ede4900ddab6c80b759de95db46c449c6
EliorRonen <Ronenelior2014@gmail.com>
2024-09-04 18:25:51 +0300

    my if statment class

---
+++ if statments.py
@@ -0,0 +1,4 @@
+islands = 999999999999
+
+if islands > 0000000000000000000000000000:
+    print(islands,"is a positive number")
---
+++ math part 2.py
@@ -0,0 +1,4 @@
+python = int(input("can you Enter the amount wich you want to withdraw"
+))
+elior = python//100
+print(elior)
